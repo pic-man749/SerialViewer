1f6c598f87163bdf9eaf0573407e37ba59b09ae7
Kase, Misao <29609700+pic-man749@users.noreply.github.com>
2025-06-24 00:18:08 +0900

    [add]show ctrl code as string

--- SerialViewer/Properties/Settings.Designer.cs
+++ SerialViewer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SerialViewer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,18 @@ namespace SerialViewer.Properties {
             }
             set {
                 this["SVLocation"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool cbReplaceCtrlChar {
+            get {
+                return ((bool)(this["cbReplaceCtrlChar"]));
+            }
+            set {
+                this["cbReplaceCtrlChar"] = value;
             }
         }
     }
